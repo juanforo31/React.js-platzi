1e5c64f32e3980d5f692b3f777102ff84d93221e
juanforo31 <juandafo31@gmail.com>
2021-04-16 21:39:30 -0500

    Organizazion del código

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Nuevo_Badge from "./paginas/Nuevo_Badge";
+//import Nuevo_Badge from "./paginas/Nuevo_Badge";
 import Badges from "./paginas/Badges";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
